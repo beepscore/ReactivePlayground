18558070cc40a35390012dffc68f4c0062a01e57
beepscore <support@beepscore.com>
2014-12-07 15:04:26 -0800

    Initial Commit

---
+++ ReactivePlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactivePlayground
+//
+//  Created by Steve Baker on 12/7/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReactivePlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactivePlayground
+//
+//  Created by Steve Baker on 12/7/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
